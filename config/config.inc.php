176e49947f2416f0a4f39c98301ec67a5c3cf6f5
MR. HASAN <hasanshahriar32@gmail.com>
2025-02-13 18:42:46 +0600

    Create config.inc.php

---
+++ config/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuration
+$_DVWA = array();
+$_DVWA['db_server'] = 'sql303.infinityfree.com';
+$_DVWA['db_user'] = 'if0_38308231';
+$_DVWA['db_password'] = 'Tasnia20';
+$_DVWA['db_database'] = 'if0_38308231_dvwa';
+$_DVWA['db_port'] = '3306';
+?>
